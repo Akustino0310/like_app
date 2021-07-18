891430cdade18ade2f3c14ead6509799c0f37322
Akustino0310 <niggy2014@yahoo.co.jp>
2021-07-18 13:01:06 +0900

    【修正】詳細画面にいいね機能を実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,7 +5,7 @@ class LikesController < ApplicationController
   end
 
   def destroy
-    current_user.likes.find_by(post_id: params[:post_id])
+    current_user.likes.find_by(post_id: params[:post_id]).destroy!
     redirect_back(fallback_location: root_path)
   end
 end
